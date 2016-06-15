5e151fb35e23b001acf3eea71d196e610d389537
Grant Burke <GB@Grants-MacBook-Pro.local>
2016-06-14 19:46:51 -0500

    Basic Fame for Webapp.

---
+++ node/shoppinglist.js
@@ -0,0 +1,16 @@
+// Main JS File of our app
+
+var express = require('express');	//Import express module 
+
+var app = express();				// Create our app object that holds our routes 
+
+app.listen(3000, function(err){		//Start listening on port 3000
+	console.log('shopplist listening on http://localhost:3000');
+});
+
+app.use('/', express.static('./app'));	// Serve our static HTML Files
+
+app.get('/list', function(req,res) {
+	res.json(["Cat Food","Dog Food","Monkey Food"]);
+});
+
